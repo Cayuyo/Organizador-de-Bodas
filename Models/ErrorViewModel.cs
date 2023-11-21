5559a7d430fe4eeb156b3b920fe45b6739501a5c
Cayuyo <cayuken00@gmail.com>
2023-11-21 13:53:53 -0300

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Organizador_de_Bodas.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
